2c6d5978ee516b630b7565b7259b94a1a83ae93c
Brett <brett_matsumoto@yahoo.com>
2019-05-06 21:46:07 -1000

    attempted route to render users page

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,9 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../database/models/users');
 
+router
+  .get('/', (req, res) => {
+    res.render('templates/users.hbs');
+  })
+
 module.exports = router;
