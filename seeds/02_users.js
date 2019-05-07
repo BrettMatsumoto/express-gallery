d3abcbc19d18339fef523c3f7e4a83fb38079703
Brett <brett_matsumoto@yahoo.com>
2019-05-06 16:55:48 -1000

    correctly named to gallery/users

---
+++ seeds/02_users.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username: 'user1', password: 'passw0rd', email: 'person1@email.com'},
+        {username: 'user2', password: 'p@ssw0rd', email: 'person2@email.com'},
+        {username: 'user3', password: 'p@$$w0rd', email: 'person3@email.com'}
+      ]);
+    });
+};
