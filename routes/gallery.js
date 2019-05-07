22bde9a3d690e2fe813f6cbc6f0735805f0a5a1a
Brett <brett_matsumoto@yahoo.com>
2019-05-06 20:04:15 -1000

    added module_exports

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../database/models/gallery');
+
+router
+  .get('/', (req, res) => {
+    res.render('index.hbs');
+  })
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../database/models/users');
+
+module.exports = router;
