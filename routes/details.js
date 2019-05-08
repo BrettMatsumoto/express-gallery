5402bbc82923558ac7d05bd3851a16f667084b8a
brett <brett_matsumoto@yahoo.com>
2019-05-07 16:23:41 -1000

    added details route

---
+++ routes/details.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const gallery = require('../database/models/gallery');
+const users = require('../database/models/users');
+
+router.get('/details', (req, res) => {
+  res.render('templates/details.hbs');
+});
+
+module.exports = router;
