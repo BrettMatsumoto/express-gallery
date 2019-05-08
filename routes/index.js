e086a64fc3caeb230d816efa4cfa5488aa1c8a78
brett <brett_matsumoto@yahoo.com>
2019-05-07 16:15:32 -1000

    added routes to reach respective pages

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../database/models/gallery');
 
-router
-  .get('/', (req, res) => {
-    res.render('index.hbs');
-  })
+router.get('/', (req, res) => {
+  res.render('index.hbs');
+});
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const knex = require('../database/models/gallery');
 
 router
-  .get('/', (req, res) => {
-    res.render('index.hbs');
+  .get('/register', (req, res) => {
+    console.log('reach register route')
+    res.render('/register.html');
   })
 
 module.exports = router;
