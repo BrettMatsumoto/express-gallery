37e37b2d822cf264a2d3640a2939a527b51c2634
brett <brett_matsumoto@yahoo.com>
2019-05-08 19:38:56 -1000

    added bookshelf

--- database/models/Gallery.js
+++ database/models/Gallery.js
@@ -3,7 +3,7 @@
 const bookshelf = require('../bookshelf');
 
 class Gallery extends bookshelf.Model {
-  get tableName() { return 'users'; }
+  get tableName() { return 'gallery'; }
   get hasTimestamps() { return true; }
 }
 
