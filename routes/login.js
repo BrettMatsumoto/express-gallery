b31eba98bb5ad9946a10effcac0c267de7efd391
brett <brett_matsumoto@yahoo.com>
2019-05-09 17:01:47 -1000

    removed console.logs

--- routes/login.js
+++ routes/login.js
@@ -10,12 +10,4 @@ router.get('/', (req, res) => {
   res.render('./templates/login.hbs');
 });
 
-// router.post('/', (req, res) => {
-//   console.log('hits login post');
-//   passport.authenticate('local', {
-//     successRedirect: '/secret',
-//     failureRedirect: '/login',
-//   })
-// })
-
 module.exports = router;
