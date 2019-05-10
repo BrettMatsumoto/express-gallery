c969b81fc0d619abee9f784edd98760758d9612e
brett <brett_matsumoto@yahoo.com>
2019-05-09 15:43:37 -1000

    added user_id to gallery seed

--- seeds/01_gallery.js
+++ seeds/01_gallery.js
@@ -2,13 +2,29 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('gallery').del()
-    .then(function () {
+  return knex('gallery')
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex('gallery').insert([
-        {author: 'McAuthor', link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct', description: 'this is first post'},
-        {author: 'McAuthor Jr', link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct', description: 'this is second post'},
-        {author: 'McAuthor Sr', link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct', description: 'this is third post'}
+        {
+          author: 'McAuthor',
+          link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',
+          description: 'this is first post',
+          user_id: 1,
+        },
+        {
+          author: 'McAuthor Jr',
+          link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',
+          description: 'this is second post',
+          user_id: 2,
+        },
+        {
+          author: 'McAuthor Sr',
+          link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',
+          description: 'this is third post',
+          user_id: 3,
+        },
       ]);
     });
 };
