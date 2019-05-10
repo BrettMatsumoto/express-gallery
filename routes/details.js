44cb3585977c811f229fe590dc364060193bfab6
brett <brett_matsumoto@yahoo.com>
2019-05-09 15:42:26 -1000

    detail route works

--- routes/details.js
+++ routes/details.js
@@ -5,8 +5,30 @@ const router = express.Router();
 const Gallery = require('../database/models/Gallery');
 const users = require('../database/models/User');
 
-router.get('/details', (req, res) => {
-  res.render('templates/details.hbs');
+router.get('/:id', (req, res) => {
+  new Gallery().fetchAll().then((result) => {
+    result = result.toJSON();
+    
+    let data = {};
+
+    for (let i = 0; i < result.length; i++) {
+      // console.log(typeof(result[i].id))
+      if (result[i].id === parseInt(req.params.id)) {
+        let bigPicture = result.splice(i, 1)[0]
+        console.log(bigPicture)
+        data = {
+          id: bigPicture.id,
+          author: bigPicture.author,
+          link: bigPicture.link,
+          description: bigPicture.description,
+          smallPictures: result,
+        };
+      }
+    }
+
+    console.log('data', data);
+    return res.render('templates/details.hbs', data);
+  });
 });
 
 module.exports = router;
