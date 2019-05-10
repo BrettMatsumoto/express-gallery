be0b0c6f9072019fd6b11090b5cda3e8caabc42f
brett <brett_matsumoto@yahoo.com>
2019-05-09 15:43:00 -1000

    index route works with hbs

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,17 @@ router.get('/', (req, res) => {
   new Gallery()
   .fetchAll()
   .then((result) => {
-    console.log(result.toJSON());
-    return res.render('index.hbs');
+    result = result.toJSON();
+    let bigPicture = result.shift();
+
+    const data = {
+      id: bigPicture.id,
+      author: bigPicture.author,
+      link: bigPicture.link,
+      description: bigPicture.description,
+      smallPictures: result
+    }
+    return res.render('index.hbs', data);
   })
 });
 
