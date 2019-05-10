f9f2aee44adeca7ca0c9e171eb75c3993fbbdbe8
brett <brett_matsumoto@yahoo.com>
2019-05-09 16:29:01 -1000

    added guard to details route

--- routes/details.js
+++ routes/details.js
@@ -2,21 +2,20 @@
 
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
+const guard = require('./middleware/guard');
 const Gallery = require('../database/models/Gallery');
 const users = require('../database/models/User');
 
-router.get('/:id', (req, res) => {
+router.get('/:id', guard, (req, res) => {
   new Gallery().fetchAll().then((result) => {
     result = result.toJSON();
     
     let data = {};
 
     for (let i = 0; i < result.length; i++) {
-      // console.log(typeof(result[i].id))
       if (result[i].id === parseInt(req.params.id)) {
         let bigPicture = result.splice(i, 1)[0]
-        console.log(bigPicture)
+
         data = {
           id: bigPicture.id,
           author: bigPicture.author,
@@ -27,13 +26,8 @@ router.get('/:id', (req, res) => {
       }
     }
 
-    console.log('data', data);
     return res.render('templates/details.hbs', data);
   });
-  // passport.authenticate('local', {
-  //   successRedirect: '/',
-  //   failureRedirect: '/login'
-  // });
 });
 
 module.exports = router;
