5fcc91b287b358de50938c7e58d639cc23e900e2
brett <brett_matsumoto@yahoo.com>
2019-05-10 15:56:01 -1000

    commented console.log

--- routes/register.js
+++ routes/register.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const knex = require('../database/models/Gallery');
 
 router.get('/', (req, res) => {
-  console.log('reached register route');
+  // console.log('reached register route');
   res.render('./templates/register.hbs');
 });
 
