1dfa7f82957f5d1e6d90451556a684994f87a497
brett <brett_matsumoto@yahoo.com>
2019-05-11 18:45:32 -1000

    remade seeds

--- seeds/01_gallery.js
+++ seeds/01_gallery.js
@@ -8,22 +8,35 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('gallery').insert([
         {
-          author: 'McAuthor',
-          link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',
-          description: 'this is first post',
-          user_id: 1
+          author: 'McAuthor Sr',
+          link: 'https://www.geirangerfjord.no/upload/images/2019viking_fjordsenter/1.jpg',
+          description: 'This is the first post',
+          user_id: 1,
         },
         {
           author: 'McAuthor Jr',
-          link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',
-          description: 'this is second post',
-          user_id: 2
+          link: 'http://www.archdaily.com/wp-content/uploads/2010/11/1290807444-fantastic-norway-3-528x316.jpg',
+          description: 'This is the second post',
+          user_id: 2,
         },
         {
-          author: 'McAuthor Sr',
-          link: 'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',
-          description: 'this is third post',
-          user_id: 3
+          author: 'McAuthor the Third',
+          link: 'https://www.designboom.com/weblog/images/images_2/lauren/mountain%20hill%20cabin/mhc01.jpg',
+          description: 'This is the third post',
+          user_id: 3,
+        },
+        {
+          author: 'McAuthor the Fourth',
+          link:
+            'https://2.bp.blogspot.com/_Ut7-u5BlpeI/TRnr4is4AFI/AAAAAAAAJZ8/1hTWyMTpSUM/s1600/Fantastic+Norway++.+Sirene+restaurant+.+Oslo.jpg',
+          description: 'This is the fourth post',
+          user_id: 4,
+        },
+        {
+          author: 'McAuthor the Fifth',
+          link: 'https://cdn.theculturetrip.com/images/56-225491-sn-hetta-1-credit-diephotodesign.de.jpg',
+          description: 'This is the Fifth post',
+          user_id: 5,
         },
       ]);
     });
--- seeds/02_users.js
+++ seeds/02_users.js
@@ -5,13 +5,16 @@ const saltRounds = 12;
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
+  return knex('users')
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex('users').insert([
-        {username: 'user1', password: bcrypt.hashSync('passw0rd', saltRounds), email: 'person1@email.com'},
-        {username: 'user2', password: bcrypt.hashSync('p@ssw0rd', saltRounds), email: 'person2@email.com'},
-        {username: 'user3', password: bcrypt.hashSync('p@$$w0rd', saltRounds), email: 'person3@email.com'}
+        { username: 'Frank', password: bcrypt.hashSync('passw0rd', saltRounds), email: 'person1@email.com' },
+        { username: 'Luke', password: bcrypt.hashSync('p@ssw0rd', saltRounds), email: 'person2@email.com' },
+        { username: 'Brenda', password: bcrypt.hashSync('p@s$w0rd', saltRounds), email: 'person3@email.com' },
+        { username: 'Brett', password: bcrypt.hashSync('p@$$w0rd', saltRounds), email: 'person4@email.com' },
+        { username: 'Ronald', password: bcrypt.hashSync('P@$$w0rd', saltRounds), email: 'person5@email.com' },
       ]);
     });
 };
